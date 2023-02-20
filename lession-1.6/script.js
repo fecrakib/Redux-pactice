43ce69b0f58b6186e07e307ea273fe1159787fb4
fecrakib <rakibislam@132575gmial.com>
2023-02-20 19:59:46 +0600

    complite

--- lession-1.6/script.js
+++ lession-1.6/script.js
@@ -1,18 +1,39 @@
+// select dom element
+const counterEl=document.getElementById("counter")
+const incrementEl=document.getElementById("increment")
+const decrementEl=document.getElementById('decrement')
+// action identifiers
+const INCREMENT="increment";
+const DECREMENT="decrement"
+// action creator
+const increment=(value)=>{
+    return{
+        type:INCREMENT,
+        playload:value,
+    }
+   
+}
+const decrement=(value)=>{
+    return{
+        type:DECREMENT,
+        playload:value
+    }
+}
 // initialState
 const initialState={
     value:0,
 }
 // create reducer function
 function counterReducer(state=initialState,action){
-if(action.type === "increment"){
+if(action.type === INCREMENT){
     return{
         ...state,
-        value:state.value+1,
+        value:state.value+action.playload,
     }
-}else if(action.type ==="decrement"){
+}else if(action.type ===DECREMENT){
     return{
         ...state,
-        value:state.value-1,
+        value:state.value-action.playload,
     }
 }else{
     return state;
@@ -22,3 +43,17 @@ if(action.type === "increment"){
 
 // create store
 const store=Redux.createStore(counterReducer);
+const render=()=>{
+    const state=store.getState();
+    counterEl.innerText=state.value.toString();
+}
+// update ui initially
+render();
+store.subscribe(render)
+// button click listeners
+incrementEl.addEventListener('click',()=>{
+    store.dispatch(increment(5))
+})
+decrementEl.addEventListener('click',()=>{
+    store.dispatch(decrement(3))
+})
